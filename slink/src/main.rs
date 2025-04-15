3bfa583a2982e987ddf258034fd421ad23ec25dd
nandi <78769380+codegod100@users.noreply.github.com>
2025-04-15 02:23:46 -0700

    ✨ feat: Improve error handling in main.rs
    Improve the error handling in the main.rs file by using the `expect()`
    method instead of `unwrap()` to provide more informative error messages
    in case of failures when creating or running the `Demo` window.
    
    Former-commit-id: c397fba45a58779e2257c567a3d33a83bf54bbfa [formerly fdafa4fad51370b3d9b995f93fdc0673fd830e72] [formerly 9447fcd0e8df064ef16842ec2993aa39631fcddd [formerly 5919dd34536711f80849b3fcc4dea9d3811bd98b]]
    Former-commit-id: 95e92e7956ff8c3035d8d50ad7cd0fb5f52058d9 [formerly df68c9be3a47a2a65c01ba86044d23ef846eef82]
    Former-commit-id: 22271b265bbde1cd9b473742e0c6b95df3580dcf

--- slink/src/main.rs
+++ slink/src/main.rs
@@ -3,8 +3,8 @@ slint::include_modules!();
 #[cfg_attr(target_arch = "wasm32",
            wasm_bindgen::prelude::wasm_bindgen(start))]
 fn main() -> Result<(), wasm_bindgen::JsValue> {
-    let main_window = Demo::new().unwrap();
+    let main_window = Demo::new().expect("Unable to create Demo");
 
-    main_window.run().unwrap();
+    main_window.run().expect("Unable to run Demo");
     Ok(())
 }
