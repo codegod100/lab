7dee149a92c019c049227db316cd5f20b77bb5e5
nandi <78769380+codegod100@users.noreply.github.com>
2025-05-22 23:40:41 -0700

    yarg
    Former-commit-id: cfee159dbedb8ab52405a5f1356d8c61a6df22de [formerly cf30a8b4dc3d5fad77872dde92975be667068ae3] [formerly a7839fd62275629e142b8d54f6e89d25f30ab05b [formerly 14cc5bf6cce9afbbf5c501848874c3fae26fcd59]]
    Former-commit-id: 2eef323482956cdf2b5cc47ff1edac35130dccc3 [formerly c2b4bf98ee04017cb499551bfb1cacc9ce3bdcb4]
    Former-commit-id: dccd40d0061227184265ba82ab8669af251b4f27

--- jarz/src/lib/schema.ts
+++ jarz/src/lib/schema.ts
@@ -2,6 +2,7 @@ import { Account, Profile, coField, co, z, Group } from "jazz-tools";
 export const MessageSchema = co.map({
     text: z.string(),
 });
+export const MessageFeedSchema = co.feed(MessageSchema)
 export const ChannelSchema = co.list(MessageSchema);
 export const SpaceSchema = co.list(ChannelSchema)
 
