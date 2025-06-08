028de52eb8088f5830a6c6a6753a7407d5264c61
nandi <nandi>
2025-06-07 22:30:09 -0700

    flict
    Former-commit-id: c800099dc19a86bb11dabe63d768fe629f5368f9 [formerly e81ec37549c5c6cf7184ab3289cfd695ed8c20f7] [formerly 6b0f0be1c64bbd6a4bc9016f9e8c46f9a1fcb36d [formerly 9a33a2b134253bb2d54623d92fe721e52d984635]]
    Former-commit-id: 9d31ad224baf4a5324f6c85f960240297faf16ab [formerly 9cbd9c89d40835ddfec05f3f789d4e2e819b383f]
    Former-commit-id: 1dba27ee737ba3363c4c87d639186c9d22a15d2c

---
+++ flict/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+    
+    // Demonstrate a simple variable and loop
+    message := "Welcome to Go programming"
+    fmt.Println(message)
+    
+    // A simple loop
+    for i := 1; i <= 3; i++ {
+        fmt.Printf("Counting: %d\n", i)
+    }
+}
